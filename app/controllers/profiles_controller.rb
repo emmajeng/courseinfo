3c643eb8e8dba8319167e4fc2b0c0d72cbf3a18c
Emma English <x15575767@student.ncirl.ie>
2019-03-26 14:31:14 +0000

    profile role added

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -88,6 +88,6 @@ class ProfilesController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def profile_params
-      params.require(:profile).permit(:firstname, :lastname, :address, :user_id)
+      params.require(:profile).permit(:firstname, :lastname, :address, :role, :user_id)
     end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20190322124750) do
+ActiveRecord::Schema.define(version: 20190326132443) do
 
   create_table "profiles", force: :cascade do |t|
     t.string   "firstname"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20190322124750) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "role"
   end
 
   add_index "profiles", ["user_id"], name: "index_profiles_on_user_id"
